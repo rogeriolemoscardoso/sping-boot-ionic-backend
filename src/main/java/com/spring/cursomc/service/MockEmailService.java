869eb5d93ba7d54649bda538ab83cb3508e865a5
Rogerio Cardoso <rogerio.lemos.cardosoo@gmail.com>
2019-10-09 00:37:33 -0300

    Email HTML

--- src/main/java/com/spring/cursomc/service/MockEmailService.java
+++ src/main/java/com/spring/cursomc/service/MockEmailService.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 
 public class MockEmailService extends AbstractEmailService {
 
@@ -12,6 +14,13 @@ public class MockEmailService extends AbstractEmailService {
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Simulando envio de email...");
+        LOG.info(msg.toString());
+        LOG.info("Email enviado");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Simulando envio de email HTML...");
         LOG.info(msg.toString());
         LOG.info("Email enviado");
     }
--- src/main/java/com/spring/cursomc/service/SmtpEmailService.java
+++ src/main/java/com/spring/cursomc/service/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -14,10 +17,21 @@ public class SmtpEmailService extends AbstractEmailService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Enviando o emaiil...");
         mailSender.send(msg);
         LOG.info("Email enviado");
     }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando o emaii HTML...");
+        javaMailSender.send(msg);
+        LOG.info("Email enviado");
+    }
 }
+
